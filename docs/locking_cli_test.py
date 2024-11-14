e0ac4fd3f0ebb38c348f041c5e95d1a5419c1c08
Fabricio C Zuardi <fabricio@fabricio.org>
2024-11-14 13:52:39 -0300

    doc improvements

--- docs/locking_cli_test.py
+++ docs/locking_cli_test.py
@@ -88,39 +88,65 @@ run_example(__name__, "locking_cli", "test_get_bucket_default_lock", config=conf
 # uma marca (delete marker). Já tentativas de deletar uma versão específica
 # de um objeto (permanent delete) são barradas pela trava.
 
+# #### Bucket versionado, com trava e um object para utilizar nos próximos exemplos
+
 @pytest.fixture
 def bucket_with_lock_and_object(active_mgc_workspace, mgc_path, bucket_with_lock):
     bucket_name = bucket_with_lock
     object_key = "key1"
-    src = "./index.md"
+    src = f"{docs_dir}/index.md"
     dst = f"{bucket_name}/{object_key}"
+
+    # Upload a file to the versioned bucket
     cmd_str = f"{mgc_path} object-storage objects upload {src} {dst}"
     result = subprocess.run(split(cmd_str), capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed with error: {result.stderr}"
 
+    # List the object versions
     cmd_str = f"{mgc_path} object-storage objects versions {dst} --raw"
     result = subprocess.run(split(cmd_str), capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed with error: {result.stderr}"
 
+    # Retrieve the only version id in the list
     versions_output = json.loads(result.stdout)
     object_version = versions_output[0].get("VersionID")
+
     return bucket_name, object_key, object_version
 
+# #### Soft delete
+#
+# Na mgc cli, um delete simples é feito com o comando `objects delete`, exemplo:
 
-def test_simple_delete_object_on_locked_bucket(active_mgc_workspace, mgc_path, bucket_with_lock_and_object):
+commands = [
+    "{mgc_path} object-storage objects delete {bucket_name}/{object_key} --no-confirm",
+]
+
+@pytest.mark.parametrize("cmd_template", commands)
+def test_simple_delete_object_on_locked_bucket(cmd_template, active_mgc_workspace, mgc_path, bucket_with_lock_and_object):
     bucket_name, object_key, _ = bucket_with_lock_and_object
 
-    cmd = split(f"{mgc_path} object-storage objects delete {bucket_name}/{object_key} --no-confirm")
+    cmd = split(cmd_template.format(mgc_path=mgc_path, bucket_name=bucket_name, object_key=object_key))
     result = subprocess.run(cmd, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed with error: {result.stderr}"
     logging.info(f"Output from {cmd}: {result.stdout}")
 
 run_example(__name__, "locking_cli", "test_simple_delete_object_on_locked_bucket", config=config, docs_dir=docs_dir)
 
-def test_permanent_delete_object_on_locked_bucket(active_mgc_workspace, mgc_path, bucket_with_lock_and_object):
+# #### Permanent delete
+#
+# Na mgc cli, um delete permanente é feito com o mesmo comando `objects delete`, porém incluindo
+# o parametro --obj-version para especificar qual versão de objeto deletar, exemplo:
+
+commands = [
+    "{mgc_path} object-storage objects delete {bucket_name}/{object_key} --no-confirm --obj-version {object_version}",
+]
+
+@pytest.mark.parametrize("cmd_template", commands)
+def test_permanent_delete_object_on_locked_bucket(cmd_template, active_mgc_workspace, mgc_path, bucket_with_lock_and_object):
     bucket_name, object_key, object_version = bucket_with_lock_and_object
 
     cmd = split(f"{mgc_path} object-storage objects delete --no-confirm {bucket_name}/{object_key}  --obj-version {object_version}")
+    cmd = split(cmd_template.format(mgc_path=mgc_path, bucket_name=bucket_name, object_key=object_key, object_version=object_version))
     result = subprocess.run(cmd, capture_output=True, text=True)
     # we do not assert the exit status of the command here because AWS may return a 200 with an AccessDenied xml inside and mgc cli will interpret it as success
     logging.info(f"Output from {cmd}: {result.stdout}")
